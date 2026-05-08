454b286898e7afc13fb5a38979c49c69d4639ec8
Nikita <ponosonik1@mail.ru>
2026-05-08 12:57:40 +0500

    chore: добавил popover в новый компонент

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import cl from "./page.module.css"
 import DSButton from "@/components/UI/Button/DSButton";
 import SomeClientComponent from "@/components/SomeClientComponent";
+import DSPopover from "@/components/UI/Popover/DSPopover";
 
 export default function Home() {
     return (
         <div className={cl.page}>
-            <h1 className={cl.title}>Home page</h1>
+            <h1 id="testst" className={cl.title}>Home page</h1>
             <DSButton variant={"primary"}>Button</DSButton>
             <DSButton variant={"secondary"}>Button</DSButton>
             <DSButton variant={"outline"}>Button</DSButton>
             <DSButton variant={"ghost"}>Button</DSButton>
+            <DSPopover parentElement="#testst" position="down-center" size="m" theme="default">
+                <p>Контент поповера</p>
+            </DSPopover>
             <DSButton variant={"destructive"}>Button</DSButton>
             <SomeClientComponent />
         </div>
